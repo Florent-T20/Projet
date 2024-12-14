822eff30df9f1a962f7df2a18a87baa8e8541e8f
Jojobizz <81922710+Fuu-kun@users.noreply.github.com>
2024-12-14 11:45:52 +0100

    Changements + ajouts
    Changements :
    - renommage "test" en "zoo"
    - déplacement d'images dans le dossier test
    - le carrousel se situe en "zoo/carousel/index.html"
    - chemins d'accès dans beaucoup de fichiers
    - le fichier "animaux.html" devra être supprimé car il est obsolète et inutile. Je le laisse pour le moment. IDEM pour "logement.html" et "gare.html" et "contact.html" qui ne servent à rien !!!
    
    Ajouts :
    - carrousel
    - images dans la base de données (table "animals")
    
    Problèmes :
    - les flèches ne fonctionnent pas correctement. Ne cliquez donc pas dessus, ça tourne automatiquement dans tous les cas
    
    Et c'est déjà pas mal. Il reste à adapter les commentaires et faire une barre de recherche en JS.
    Lisez les commits aussi svp. Je sais que vous ne le faites pas et c'est très chiant.

--- BDD/projet_web_2425.sql
+++ BDD/projet_web_2425.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 09 déc. 2024 à 14:08
+-- Généré le : sam. 14 déc. 2024 à 10:35
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -37,93 +37,86 @@ CREATE TABLE IF NOT EXISTS `animals` (
   `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`animal_id`),
   KEY `enclosure_id` (`enclosure_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=85 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `animals`
 --
 
 INSERT INTO `animals` (`animal_id`, `name`, `species`, `age`, `enclosure_id`, `image`) VALUES
-(1, 'Raoul', 'Ara perroquet', 1, 9, NULL),
-(2, 'Chaussette', 'Panthère', 3, 7, NULL),
-(3, 'Zgeg', 'Grand hocco', 1, 8, NULL),
-(4, 'Jeannot', 'Panda roux', 1, 1, NULL),
-(5, 'Dimitri', 'Lémurien', 1, 2, NULL),
-(6, 'Becamerde', 'Chèvre naine', 2, 3, NULL),
-(7, 'Poilu', 'Mouflon', 3, 4, NULL),
-(8, 'Carapute', 'Tortue', 10, 6, NULL),
-(9, 'Michel', 'Cerf', 5, 11, NULL),
-(10, 'Mouloud', 'Macaque crabier', 3, 10, NULL),
-(11, 'Zgeg', 'Vautour', 2, 12, NULL),
-(12, 'Jeannine', 'Antilope', 4, 13, NULL),
-(13, 'Maria', 'Daim', 3, 13, NULL),
-(14, 'Becamerde', 'Nilgaut', 2, 13, NULL),
-(15, 'Oeil percé', 'Loup d\'\'Europe', 3, 14, NULL),
-(16, 'Griffe sanglante', 'Loup d\'\'Europe', 4, 14, NULL),
-(17, 'Pauline', 'Cigogne', 1, 15, NULL),
-(18, 'Mohamed', 'Marabout', 3, 15, NULL),
-(19, 'Patatartiné', 'Oryx algazelle', 2, 16, NULL),
-(20, 'Gérard', 'Watusi', 4, 16, NULL),
-(21, 'Moquette', 'Âne de Somalie', 4, 16, NULL),
-(22, 'Aubergine', 'Yack', 2, 17, NULL),
-(23, 'Bouldepoil', 'Mouton noir', 3, 17, NULL),
-(24, 'Sapik', 'Porc-épic', 4, 18, NULL),
-(25, 'Meumeu', 'Emeu', 5, 19, NULL),
-(26, 'Joharno', 'Wallaby', 3, 19, NULL),
-(27, 'Budget', 'Ibis', 2, 20, NULL),
-(28, 'Carabosse', 'Tortue', 4, 20, NULL),
-(29, 'Mario', 'Pécari', 3, 21, NULL),
-(30, 'Luigi', 'Lynx', 2, 22, NULL),
-(31, 'Gris', 'Cerval', 3, 23, NULL),
-(32, 'Ouaf', 'Chien des buissons', 4, 24, NULL),
-(33, 'Tigrou', 'Tigre', 4, 25, NULL),
-(34, 'Isabelle', 'Flamant rose', 1, 26, NULL),
-(35, 'Lemanoir', 'Lamanoir', 4, 26, NULL),
-(36, 'Théo', 'Nandou', 3, 26, NULL),
-(37, 'Buffalo', 'Bison', 7, 27, NULL),
-(38, 'Chameau', 'Dromadaire', 4, 28, NULL),
-(39, 'Triton', 'Âne de provence', 5, 28, NULL),
-(40, 'Cinquante', 'Zèbre', 3, 29, NULL),
-(41, 'Ed', 'Hyène', 2, 30, NULL),
-(42, 'Diabétik', 'Hippopotame', 4, 31, NULL),
-(43, 'Waluigi', 'Loup à crinière', 3, 32, NULL),
-(44, 'Dumbo', 'Eléphant', 11, 33, NULL),
-(45, 'Parchemin', 'Varan de Komodo', 3, 34, NULL),
-(46, 'Longitude', 'Girafe', 4, 35, NULL),
-(47, 'Gru', 'Grivet cercopithèque', 4, 35, NULL),
-(48, 'Gege', 'Tamarin', 1, 36, NULL),
-(49, 'Ouioui', 'Ouistiti', 4, 37, NULL),
-(50, 'Théophile', 'Gibbon', 3, 38, NULL),
-(51, 'Capucine', 'Capucin', 3, 39, NULL),
-(52, 'Simba', 'Lion', 4, 40, NULL),
-(53, 'Tapis', 'Tapir', 3, 41, NULL),
-(54, 'Caesar', 'Casoar', 2, 42, NULL),
-(55, 'Sacourvite', 'Guépard', 4, 43, NULL),
-(56, 'Crocro', 'Crocodile nain', 3, 44, NULL),
-(57, 'Biscotte', 'Gazelle', 11, 45, NULL),
-(58, 'Gépeur', 'Autruche', 3, 45, NULL),
-(59, 'Genou', 'Gnou', 4, 46, NULL),
-(60, 'Onyx', 'Oryx beisa', 4, 46, NULL),
-(61, 'Suricate', 'Rhinocéros', 3, 47, NULL),
-(62, 'Sable', 'Fennec', 4, 48, NULL),
-(63, 'Manteau', 'Coati', 3, 49, NULL),
-(64, 'Tapidesouris', 'Tapir', 3, 41, NULL),
-(65, 'Caesaro', 'Casoar', 2, 42, NULL),
-(66, 'Olalaa', 'Guépard', 4, 43, NULL),
-(67, 'Croks', 'Crocodile nain', 3, 44, NULL),
-(68, 'Cracotte', 'Gazelle', 11, 45, NULL),
-(69, 'Gemini', 'Autruche', 3, 45, NULL),
-(70, 'Rotule', 'Gnou', 4, 46, NULL),
-(71, 'Xynno', 'Oryx beisa', 4, 46, NULL),
-(72, 'Suripussy', 'Rhinocéros', 3, 47, NULL),
-(73, 'Kayou', 'Fennec', 4, 48, NULL),
-(74, 'Veste', 'Coati', 3, 49, NULL),
-(75, 'Samaritin', 'Saïmiri', 1, 49, NULL),
-(76, 'Jigot', 'Iguane', 1, 50, NULL),
-(77, 'Viper', 'Python', 2, 50, NULL),
-(78, 'Carabosse', 'Tortue', 10, 50, NULL),
-(79, 'Jertrude', 'Lémurien', 2, 2, NULL),
-(80, 'Bicyclette', 'Panda roux', 1, 1, NULL);
+(1, 'Raoul', 'Ara perroquet', 1, 9, 'images/animaux/jpg/AraPerroquet.jpg'),
+(2, 'Chaussette', 'Panthère', 3, 7, 'images/animaux/jpg/Panthere.jpg'),
+(3, 'Zgeg', 'Grand hocco', 1, 8, 'images/animaux/jpg/GrandHocco.jpg'),
+(4, 'Jeannot', 'Panda roux', 1, 1, 'images/animaux/jpg/PandaRoux.jpg'),
+(5, 'Dimitri', 'Lémurien', 1, 2, 'images/animaux/jpg/Lemurien.jpg'),
+(6, 'Becamerde', 'Chèvre naine', 2, 3, 'images/animaux/jpg/ChevreNaine.jpg'),
+(7, 'Poilu', 'Mouflon', 3, 4, 'images/animaux/jpg/Mouflon.jpg'),
+(8, 'Carapute', 'Tortue', 10, 6, 'images/animaux/jpg/Tortue.jpg'),
+(9, 'Michel', 'Cerf', 5, 11, 'images/animaux/jpg/Cerf.jpg'),
+(10, 'Mouloud', 'Macaque crabier', 3, 10, 'images/animaux/jpg/MacaqueCrabier.jpg'),
+(11, 'Zgeg', 'Vautour', 2, 12, 'images/animaux/jpg/Vautour.jpg'),
+(12, 'Jeannine', 'Antilope', 4, 13, 'images/animaux/jpg/Antilope.jpg'),
+(13, 'Maria', 'Daim', 3, 13, 'images/animaux/jpg/Daim.jpg'),
+(14, 'Becamerde', 'Nilgaut', 2, 13, 'images/animaux/jpg/Nilgaut.jpg'),
+(15, 'Oeil percé', 'Loup d\'\'Europe', 3, 14, 'images/animaux/jpg/LoupdEurope.jpg'),
+(16, 'Griffe sanglante', 'Loup d\'\'Europe', 4, 14, 'images/animaux/jpg/LoupdEurope.jpg'),
+(17, 'Pauline', 'Cigogne', 1, 15, 'images/animaux/jpg/Cigogne.jpg'),
+(18, 'Mohamed', 'Marabout', 3, 15, 'images/animaux/jpg/Marabout.jpg'),
+(19, 'Patatartiné', 'Oryx algazelle', 2, 16, 'images/animaux/jpg/OryxAlgazelle.jpg'),
+(20, 'Gérard', 'Watusi', 4, 16, 'images/animaux/jpg/Watusi.jpg'),
+(21, 'Moquette', 'Âne de Somalie', 4, 16, 'images/animaux/jpg/AneDeSomalie.jpg'),
+(22, 'Aubergine', 'Yack', 2, 17, 'images/animaux/jpg/Yack.jpg'),
+(23, 'Bouldepoil', 'Mouton noir', 3, 17, 'images/animaux/jpg/MoutonNoir.jpg'),
+(24, 'Sapik', 'Porc-épic', 4, 18, 'images/animaux/jpg/PorcEpic.jpg'),
+(25, 'Meumeu', 'Emeu', 5, 19, 'images/animaux/jpg/Emeu.jpg'),
+(26, 'Joharno', 'Wallaby', 3, 19, 'images/animaux/jpg/Wallaby.jpg'),
+(27, 'Budget', 'Ibis', 2, 20, 'images/animaux/jpg/Ibis.jpg'),
+(28, 'Carabosse', 'Tortue', 4, 20, 'images/animaux/jpg/Tortue.jpg'),
+(29, 'Mario', 'Pécari', 3, 21, 'images/animaux/jpg/Pecari.jpg'),
+(30, 'Luigi', 'Lynx', 2, 22, 'images/animaux/jpg/Lynx.jpg'),
+(31, 'Gris', 'Serval', 3, 23, 'images/animaux/jpg/Serval.jpg'),
+(32, 'Ouaf', 'Chien des buissons', 4, 24, 'images/animaux/jpg/ChienDesBuissons.jpg'),
+(33, 'Tigrou', 'Tigre', 4, 25, 'images/animaux/jpg/Tigre.jpg'),
+(34, 'Isabelle', 'Flamant rose', 1, 26, 'images/animaux/jpg/FlamantRose.jpg'),
+(35, 'Lemanoir', 'Tamanoir', 4, 26, 'images/animaux/jpg/Tamanoir.jpg'),
+(36, 'Théo', 'Nandou', 3, 26, 'images/animaux/jpg/Nandou.jpg'),
+(37, 'Buffalo', 'Bison', 7, 27, 'images/animaux/jpg/Bison.jpg'),
+(38, 'Chameau', 'Dromadaire', 4, 28, 'images/animaux/jpg/Dromadaire.jpg'),
+(39, 'Triton', 'Âne de provence', 5, 28, 'images/animaux/jpg/AneDeProvence.jpg'),
+(40, 'Cinquante', 'Zèbre', 3, 29, 'images/animaux/jpg/Zebre.jpg'),
+(41, 'Ed', 'Hyène', 2, 30, 'images/animaux/jpg/Hyene.jpg'),
+(42, 'Diabétik', 'Hippopotame', 4, 31, 'images/animaux/jpg/Hippopotame.jpg'),
+(43, 'Waluigi', 'Loup à crinière', 3, 32, 'images/animaux/jpg/LoupACriniere.jpg'),
+(44, 'Dumbo', 'Eléphant', 11, 33, 'images/animaux/jpg/Elephant.jpg'),
+(45, 'Parchemin', 'Varan de Komodo', 3, 34, 'images/animaux/jpg/VaranDeKomodo.jpg'),
+(46, 'Longitude', 'Girafe', 4, 35, 'images/animaux/jpg/Girafe.jpg'),
+(47, 'Gru', 'Grivet cercopithèque', 4, 35, 'images/animaux/jpg/GrivetCercopitheque.jpg'),
+(48, 'Gege', 'Tamarin', 1, 36, 'images/animaux/jpg/Tamarin.jpg'),
+(49, 'Ouioui', 'Ouistiti', 4, 37, 'images/animaux/jpg/Ouistiti.jpg'),
+(50, 'Théophile', 'Gibbon', 3, 38, 'images/animaux/jpg/Gibbon.jpg'),
+(51, 'Capucine', 'Capucin', 3, 39, 'images/animaux/jpg/Capucin.jpg'),
+(52, 'Simba', 'Lion', 4, 40, 'images/animaux/jpg/Lion.jpg'),
+(54, 'Caesar', 'Casoar', 2, 42, 'images/animaux/jpg/Casoar.jpg'),
+(55, 'Sacourvite', 'Guépard', 4, 43, 'images/animaux/jpg/Guepard.jpg'),
+(56, 'Crocro', 'Crocodile nain', 3, 44, 'images/animaux/jpg/CrocodileNain.jpg'),
+(57, 'Biscotte', 'Gazelle', 11, 45, 'images/animaux/jpg/Gazelle.jpg'),
+(58, 'Gépeur', 'Autruche', 3, 45, 'images/animaux/jpg/Autruche.jpg'),
+(60, 'Onyx', 'Oryx beisa', 4, 46, 'images/animaux/jpg/OryxBeisa.jpg'),
+(61, 'Suricate', 'Rhinocéros', 3, 46, 'images/animaux/jpg/Rhinoceros.jpg'),
+(62, 'Sable', 'Fennec', 4, 48, 'images/animaux/jpg/Fennec.jpg'),
+(63, 'Manteau', 'Coati', 3, 49, 'images/animaux/jpg/Coati.jpg'),
+(64, 'Tapidesouris', 'Tapir', 3, 41, 'images/animaux/jpg/Tapir.jpg'),
+(65, 'Caesaro', 'Casoar', 2, 42, 'images/animaux/jpg/Casoar.jpg'),
+(70, 'Rotule', 'Gnou', 4, 46, 'images/animaux/jpg/Gnou.jpg'),
+(75, 'Samaritin', 'Saïmiri', 1, 49, 'images/animaux/jpg/Saimiri.jpg'),
+(76, 'Jigot', 'Iguane', 1, 50, 'images/animaux/jpg/Iguane.jpg'),
+(78, 'Carapace', 'Tortue', 10, 50, 'images/animaux/jpg/Tortue.jpg'),
+(79, 'Jertrude', 'Lémurien', 2, 2, 'images/animaux/jpg/Lemurien.jpg'),
+(80, 'Bicyclette', 'Panda roux', 1, 1, 'images/animaux/jpg/PandaRoux.jpg'),
+(83, 'C++', 'Python', 3, 50, 'images/animaux/jpg/Python.jpg'),
+(84, 'Debousurpatte', 'Suricate', 3, 47, 'images/animaux/jpg/Suricate.jpg'),
+(81, 'Johnny', 'Loutre', 2, 5, 'images/animaux/jpg/Loutre.jpg'),
+(82, 'Casspatou', 'Binturong', 3, 5, 'images/animaux/jpg/Binturong.jpg');
 
 -- --------------------------------------------------------
 
@@ -196,7 +189,7 @@ INSERT INTO `enclosures` (`enclosure_id`, `name`, `description`, `status`, `biom
 (20, 'Enclos des ibis et tortues', NULL, 'open', 3),
 (21, 'Enclos des pécaris', NULL, 'open', 3),
 (22, 'Enclos des lynxs', NULL, 'open', 3),
-(23, 'Enclos des cervals', NULL, 'open', 3),
+(23, 'Enclos des servals', NULL, 'open', 3),
 (24, 'Enclos des chiens des buissons', NULL, 'open', 3),
 (25, 'Enclos des tigres', NULL, 'open', 3),
 (26, 'Enclos des flamants roses, tamanoirs et nandou', NULL, 'open', 3),
@@ -255,7 +248,9 @@ INSERT INTO `reviews` (`review_id`, `user_id`, `enclosure_id`, `rating`, `commen
 (11, NULL, NULL, 2, 'test', '2024-12-09 12:48:42'),
 (14, NULL, NULL, 1, 'éclaté au sol', '2024-12-09 13:02:12'),
 (15, NULL, NULL, 5, 'test\r\n  test tt\r\n     test\r\n\r\nlolp', '2024-12-09 13:17:33'),
-(16, 3, NULL, 5, 'SUPER GENIAL\r\nありがとう', '2024-12-09 13:27:18');
+(16, 3, NULL, 5, 'SUPER GENIAL\r\nありがとう', '2024-12-09 13:27:18'),
+(20, 5, NULL, 5, 'super', '2024-12-11 21:08:43'),
+(19, NULL, NULL, 3, 'gros caca', '2024-12-11 21:04:47');
 
 -- --------------------------------------------------------
 
@@ -332,7 +327,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `users`
@@ -340,7 +335,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `role`, `created_at`) VALUES
 (5, 'admin', '$2y$10$EmeAhNCYRE6tzz/d/BWEWuRTUAbiaq/.Oa8DfkUjH592rfP.0IDiy', 'mathys.rageade@isen.yncrea.fr', 'admin', '2024-11-29 17:00:38'),
-(3, 'mathys', '$2y$10$1SOF2dhokke5VwvBBY/WiOkSoHXeHKDIoZYqhreUjSbMD4aUFdjIC', 'bloup@superkk.prout', 'user', '2024-11-19 14:09:17');
+(3, 'mathys', '$2y$10$1SOF2dhokke5VwvBBY/WiOkSoHXeHKDIoZYqhreUjSbMD4aUFdjIC', 'bloup@superkk.prout', 'user', '2024-11-19 14:09:17'),
+(6, 'thomas', '$2y$10$CG2WDVgNmVzK7EFzEvmi2edQL0WVLbxRzbuDwxsj1yw41zqciYW/q', 'thomas@caca.fr', 'user', '2024-12-11 21:07:19');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
